bd49f8a1fb298389a0cdecf077f2ffc50cad79fc
Supavate <supavate@gmail.com>
2024-02-08 15:47:56 +0700

    refactor 2 : calculate function

--- common.js
+++ common.js
@@ -3,22 +3,32 @@ function calculate(p,d){
 		var pace = parseFloat(p);
 		var dist = parseInt(d);
 	//---------------------------
-		if(isNaN(pace)){
-			msg = "Please verify that the pace you have entered is correct!!!";
-			return {"status":0,"message":msg,"hour":0,"min":0};
-		}
-		if(!(pace >= 3 && pace <= 10)){
-			msg = "Running pace must be between 3 and 10 min/km";
-			return {"status":0,"message":msg,"hour":0,"min":0};
-		}
+		var check = vaildate(pace);
+		if (check!=null) return check;
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+		var cal = calculate(dist, pace);
 	//---------------------------
-		msg = "successfully calculated!"
+		msg = "successfully calculated!";
 		return {"status":1,"message":msg,"hour":hour,"min":min};
+}
+
+function vaildate(pace) {
+	if(isNaN(pace)){
+		msg = "Please verify that the pace you have entered is correct!!!";
+		return {"status":0,"message":msg,"hour":0,"min":0};
+	}
+	if(!(pace >= 3 && pace <= 10)){
+		msg = "Running pace must be between 3 and 10 min/km";
+		return {"status":0,"message":msg,"hour":0,"min":0};
+	}
+}
+
+function calculate(dist, pace) {
+	var min = dist * pace;
+	var hour = 0;
+	if(min > 60){
+		hour = Math.floor(min / 60);          
+		min = min % 60;
+	}
+	return {"min":min,"hour":hour};
 }
